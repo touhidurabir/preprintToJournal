a7c63e09b14895678d637cf4c1cc73d30e08c85e
Touhidur Rahman <abir.ruet@gmail.com>
2023-11-24 17:17:49 +0600

    working on transferring

--- controllers/JournalSubmissionHandler.php
+++ controllers/JournalSubmissionHandler.php
@@ -2,17 +2,23 @@
 
 namespace APP\plugins\generic\preprintToJournal\controllers;
 
-use APP\core\Application;
-use APP\handler\Handler;
+use Throwable;
 use APP\core\Request;
 use APP\facades\Repo;
-use APP\plugins\generic\preprintToJournal\PreprintToJournalPlugin;
-use APP\plugins\generic\preprintToJournal\classes\models\RemoteService;
+use APP\core\Services;
 use PKP\config\Config;
+use PKP\security\Role;
 use PKP\facades\Locale;
+use APP\handler\Handler;
 use PKP\context\Context;
+use APP\core\Application;
+use Illuminate\Support\Str;
 use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
+use PKP\security\authorization\UserRequiredPolicy;
+use PKP\security\authorization\RoleBasedHandlerOperationPolicy;
+use APP\plugins\generic\preprintToJournal\PreprintToJournalPlugin;
+use APP\plugins\generic\preprintToJournal\classes\models\RemoteService;
 
 class JournalSubmissionHandler extends Handler
 {
@@ -25,6 +31,9 @@ class JournalSubmissionHandler extends Handler
     
     public function authorize($request, &$args, $roleAssignments)
     {
+        // $this->addPolicy(new UserRequiredPolicy($request), true);
+        // $this->addPolicy(new RoleBasedHandlerOperationPolicy($request, [Role::ROLE_ID_AUTHOR], ['confirmJournalTransfer']));
+
         return parent::authorize($request, $args, $roleAssignments);
     }
 
@@ -91,9 +100,66 @@ class JournalSubmissionHandler extends Handler
         ], Response::HTTP_OK)->send();
     }
 
-    public function confirmJournalTransfer(array $args, Request $request): JsonResponse
+    public function confirmJournalTransfer(array $args, Request $request)
     {
+        // 1. check if user is logged in
+        // 2. if not, redirect to login
+        // 3. if logged in/after login, start moving the submission
+        $remoteService = RemoteService::find($request->getUserVar('serviceId'));
         
+        if (!$remoteService) {
+            return response()->json([
+                'message' => 'Service not found',
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $contextService = Services::get('context'); /** @var \APP\services\ContextService $contextService */
+        $context = $contextService->get((int)$remoteService->context_id); /** @var \App\server\Server $context */
+
+        $serverPath = last(explode('/', $remoteService->url));
+        $journalConfigDetailsUrl = Str::of(
+            $request->getDispatcher()->url(
+                $request,
+                Application::ROUTE_COMPONENT,
+                $serverPath,
+                'plugins.generic.preprintToJournal.controllers.JournalPublishingHandler',
+                'getTransferableJournalDetails',
+                null,
+                ['submissionId' => $request->getUserVar('uuid'), 'serviceId' => $remoteService->remote_service_id]
+            )
+        )
+        ->replace($request->getBaseUrl() . '/index.php/' . $context->getData('urlPath'), $remoteService->url)
+        ->__toString();
+        
+        $httpClient = Application::get()->getHttpClient();
+        $header = [
+            'Accept'    => 'application/json',
+        ];
+
+        try {
+            $response = $httpClient->request(
+                'POST',
+                $journalConfigDetailsUrl,
+                [
+                    'http_errors'   => false,
+                    'headers'       => $header,
+                ]
+            );
+
+            if ($response->getStatusCode() === Response::HTTP_OK) {
+
+                $data = json_decode($response->getBody(), true)['data'];
+            }
+
+            return false;
+
+        } catch(Throwable $exception) {
+
+            ray($exception);
+        }
+        // 4. once the moving complete/failed, send a coar notification (LDN notification)
+        // 5. May be notify OPS end via other means about the result if the LDN notification is not sufficient
+        // 6. once submission done moving, redirect to submission wizard of transfered submission
     }
 
     protected function getLocaleOptions(Context $context): array
